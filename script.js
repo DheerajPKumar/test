8b0e5400dbc08baaf66c6107977ea48252e4c5a2
DheerajPKumar <dheeraj.kumar@axeno.co>
2024-08-05 13:44:13 +0530

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+const colorDiv = Array.from(document.getElementsByClassName("colorDiv"));
+// colorDiv[0].style.backgroundColor = "red";
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+
+// btn1.addEventListener("click", func);
+
+// function func() {
+//      let c1 = (Math.round(), Math.random(), 255);
+//      let c2 = (Math.round(), Math.random(), 255);
+//      let c3 = (Math.round(), Math.random(), 255);
+//      colorDiv[0].style.backgroundColor = "blue";
+// }
+
+const parent = document.getElementsByClassName("container");
+const childDiv = document.createElement("div");
+document.body.appendChild(childDiv);
+btn2.addEventListener("click", func);
+
+// function func(){
+//      const childdiv = document.createElement("div");
+// }
